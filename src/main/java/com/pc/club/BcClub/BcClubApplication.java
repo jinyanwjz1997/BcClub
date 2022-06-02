c36c79246ae264eb6673a14e0154f0784108c1df
jinyWei <122857147@qq.com>
2022-06-02 14:37:09 +0800

    1.init 2.取消数据库连接

--- src/main/java/com/pc/club/BcClub/BcClubApplication.java
+++ src/main/java/com/pc/club/BcClub/BcClubApplication.java
@@ -3,11 +3,14 @@ package com.pc.club.BcClub;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+//@SpringBootApplication
 @EnableScheduling
 @MapperScan("com.pc.club.BcClub.mapper")
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class BcClubApplication {
 
 	public static void main(String[] args) {
